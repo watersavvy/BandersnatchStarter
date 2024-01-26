56c1a18da8b87c378b6bf8a85a5fab0d37374397
Angelique Kepshire <akepshire@gmail.com>
2024-01-26 11:56:57 -0600

    Added the Database structure to produce a table of Monsters with various attributes.

--- app/data.py
+++ app/data.py
@@ -7,19 +7,49 @@ from pandas import DataFrame
 from pymongo import MongoClient
 
 
-class Database:
+class MongoDB:
+
+    load_dotenv()
+    database = MongoClient(getenv("DB_URL"), tlsCAFile=where())["Database"]
+
+    def __init__(self, collection: str):
+        """Initializes the Database class with the specified collection.
+
+        Args:
+            collection (str): The name of the collection to work with, passed as string.
+        """
+        self.collection = self.database[collection]
 
     def seed(self, amount):
-        pass
+
+        # make a new empty list (append)
+        self.collection.insert_many(
+            [Monster().to_dict() for i in range(amount)])
+
+        return f"Successfully inserted {amount} documents"
 
     def reset(self):
-        pass
+        """
+        Erase all records in the collection
+        """
+        return {"Collection reset successfully": f"{self.collection.delete_many(filter={}).acknowledged}"}
 
     def count(self) -> int:
-        pass
+        """
+        Return the number of records in the collection.
+        """
+        return self.collection.count_documents(filter={})
 
     def dataframe(self) -> DataFrame:
-        pass
+        """
+        Return a DataFrame containing all records in the collection
+        """
+
+        return DataFrame(self.collection.find(dict(), {"_id": False}))
 
     def html_table(self) -> str:
-        pass
+        """
+        Return an HTML table containing all records in the collection
+        """
+        df = self.dataframe()
+        return df.html
